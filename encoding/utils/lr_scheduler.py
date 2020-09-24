5d67aaed0d46e723c4f6a91412d71d1c310185dd
sun <sun@sun>
2020-09-24 19:23:43 +0800

    debug

--- encoding/utils/lr_scheduler.py
+++ encoding/utils/lr_scheduler.py
@@ -60,7 +60,7 @@ class LR_Scheduler(object):
             raise NotImplemented
         if epoch > self.epoch and (epoch == 0 or best_pred > 0.0):
             if not self.quiet:
-                print('\n=>Epoch %i, learning rate = %f, \
+                print('\n=>Epoch %i, learning rate = %.10f, \
                     previous best = %.4f' % (epoch, lr, best_pred))
             self.epoch = epoch
         assert lr >= 0
