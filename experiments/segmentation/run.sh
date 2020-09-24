e849cae21deddc5929c26f35318074f1175bb621
sun <sun@sun>
2020-09-24 19:15:36 +0800

    warm epoch 1

--- experiments/segmentation/run.sh
+++ experiments/segmentation/run.sh
@@ -7,4 +7,7 @@ python train_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest26
 
 python test_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest269 --resume runs/sar_voc/deeplab/resnest269/default/checkpoint.pth.tar --eval
 
-# 15 0.5315
+# 15 no warm 0.5315
+# 15 warm    
+# 20 no warm 0.5286
+# 20 warm    
