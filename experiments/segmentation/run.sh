df65f933ed480c92f8a06b26a61ef9fb24b906f5
sun <sun@sun>
2020-09-25 15:53:10 +0800

    train

--- experiments/segmentation/run.sh
+++ experiments/segmentation/run.sh
@@ -3,19 +3,19 @@ python setup.py install
 
 cd experiments/segmentation
 
-python train_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest269 --batch-size 30 --epochs 10 --warmup-epochs 0
+python train_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest269 --batch-size 30 --epochs 15 --warmup-epochs 0
 
 python test_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest269 --resume runs/sar_voc/deeplab/resnest269/default/model_best.pth.tar --eval
 
 # lr 0.0001
 
-# 10 no warm           
+# 10 no warm        0.3237 10   
 # 15 no warm           
 # 15 warm    
 # 20 no warm 
 # 20 warm       1      
 # 30 warm       1      
-# 40 warm       2      
+# 40 warm       2   0.4546    
 
 
 # adam
