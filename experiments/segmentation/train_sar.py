0859980ba3a7172f7c55f00eb5496e31b16ad120
sun <sun@sun>
2020-09-25 17:37:42 +0800

    debug

--- experiments/segmentation/train_sar.py
+++ experiments/segmentation/train_sar.py
@@ -262,8 +262,8 @@ class Trainer():
 
             tbar.set_description(
                 'pixAcc: %.3f, mIoU: %.3f, fwIoU: %.3f \n\
-                 freq0: %.3f, freq1: %.3f, freq2: %.3f, freq3: %.3f, freq4: %.3f, freq5: %.3f, freq6: %.3f \n\
-                 IoU0: %.3f, IoU1: %.3f, IoU2: %.3f, IoU3: %.3f, IoU4: %.3f, IoU5: %.3f, IoU6: %.3f \n'\
+                freq0: %.3f, freq1: %.3f, freq2: %.3f, freq3: %.3f, freq4: %.3f, freq5: %.3f, freq6: %.3f \n\
+                IoU0: %.3f, IoU1: %.3f, IoU2: %.3f, IoU3: %.3f, IoU4: %.3f, IoU5: %.3f, IoU6: %.3f'\
                      % (pixAcc, mIoU, fwIoU, freq[0], freq[1], freq[2], freq[3], freq[4], freq[5], freq[6], \
                         IoU[0], IoU[1], IoU[2], IoU[3], IoU[4], IoU[5], IoU[6] ))
 
