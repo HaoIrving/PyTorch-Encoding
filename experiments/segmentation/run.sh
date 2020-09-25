5ff01b1fcfaeccbee6668253c712de5732ef1182
sun <sun@sun>
2020-09-25 21:02:23 +0800

    50

--- experiments/segmentation/run.sh
+++ experiments/segmentation/run.sh
@@ -3,23 +3,20 @@ python setup.py install
 
 cd experiments/segmentation
 
-python train_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest269 --batch-size 30 --epochs 30 --warmup-epochs 0
-
-python test_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest269 --resume runs/sar_voc/deeplab/resnest269/default/model_best.pth.tar --eval
-
-# lr 0.0001
-        # no warm       warm 1      warm 2
-# 10    0.3237 10   
-# 15    0.3430 15       0.3445      0.3469
-# 20    0.3533 20       0.3574 20   
-# 30    
-# 40    
-
-
-# 15 warm    
-# 20 warm       1      
-# 30 warm       1      
-# 40 warm       2   0.4546    
+# python train_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest269 --batch-size 30 --epochs 30 --warmup-epochs 0
+# python test_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest269 --resume runs/sar_voc/deeplab/resnest269/default/model_best.pth.tar --eval
+# # lr 0.0001
+#         # no warm       warm 1      warm 2
+# # 20    0.3533 20       0.3574 20   
+# # 30    
+# # 40    
+python train_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest50 --batch-size 30 --epochs 30 --warmup-epochs 0
+python test_sar.py --dataset sar_voc --model deeplab --aux --backbone resnest50 --resume runs/sar_voc/deeplab/resnest50/default/model_best.pth.tar --eval
+# # lr 0.0001
+#         # no warm       warm 1      warm 2
+# # 20    
+# # 30    
+# # 40 
 
 
 # adam
