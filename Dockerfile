35b514943e52b30d65799788a540476673305d79
sun <sun@sun>
2020-10-01 21:52:28 +0800

    build docker

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,25 @@
-FROM nvcr.io/nvidia/pytorch:20.06-py3
+# 基础的镜像，如果使用cuda，需要满足cuda为10.0，ubuntu16.04可以换成Centos或者ubuntu18.04；
+FROM conda-segdc:2.0
 
-# Set working directory	# Set working directory
 WORKDIR /workspace
 
-#ENV PYTHONPATH "${PYTHONPATH}:/workspace"
+# 将程序复制容器内，表示在/workspace路径下
+COPY ./model_best.pth.tar  .
+COPY ./resnest269-51ae5f19.lock .
+COPY ./resnest269-51ae5f19.pth .
+COPY ./test_sar_docker.py .
+# RUN python setup.py install
 
-RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y python3-tk python-pip git tmux htop tree
-
-RUN python -m pip install --upgrade pip
-#RUN python -m pip install torch==1.4.0
-#RUN python -m pip install torchvision==0.5.0
-RUN python -m pip install pycocotools==2.0.0
-
-#RUN chmod a+rwx -R /opt/conda/
-
+COPY ./README.md .
 COPY ./setup.py .
 COPY ./encoding ./encoding
+COPY ./input_path ./input_path
+# # 确定容器启动时程序运行路径
+# WORKDIR /workspace
 
-ENV FORCE_CUDA="1"
-RUN python setup.py develop
+# RUN source activate segdc
+# ENV FORCE_CUDA="1"
+# RUN python setup.py install
 
-COPY ./experiments ./experiments
+# 确定容器启动命令。这里列举python示例，python表示编译器，xxx.py表示执行文件，input_path和output_path为容器内绝对路径，测评时会自动将测试数据挂载到容器内/input_path路径，不需要修改；
+CMD ["python", "test_sar_docker.py", "/input_path", "/output_path"]
--- experiments/segmentation/run.sh
+++ experiments/segmentation/run.sh
@@ -5,7 +5,7 @@ cd experiments/segmentation
 
 
 python train_sar.py --dataset sar_voc --child log_normal_new_noise_c1 --model deeplab --aux --backbone resnest269 \
---batch-size 30 --epochs 300 --warmup-epochs 2 
+--batch-size 30 --epochs 500 --warmup-epochs 2 
 # python train_sar.py --dataset sar_voc --child log_normal_new_c1 --model deeplab --aux --backbone resnest269 \
 # --batch-size 30 --epochs 100 --warmup-epochs 2 --resume runs/sar_voc/deeplab/resnest269/default/model_best.pth.tar
 # python train_sar.py --dataset sar_voc --child log_normal_new_c1 --model deeplab --aux --backbone resnest269 \
@@ -18,12 +18,14 @@ python test_sar.py --dataset sar_voc --child log_normal_new_noise_c1 --model dee
 # # 30    0.3710 2      0.4230 27           0.4255 30
 # # 100                 0.5390 97
 # # 200                 0.5792 184
-# # 300                 0.5905 284
+# # 300                 0.5905 284          0.5999 293
 
 # pixAcc: 0.7402, mIoU: 0.5381, fwIoU: 0.5905: 100%|������������������������������������������������������������������������������������������������������| 17/17 [00:45<00:00,  2.66s/it]
 # freq0: 0.061955, freq1: 0.078376, freq2: 0.163651, freq3: 0.357225, freq4: 0.298697, freq5: 0.016080, freq6: 0.024017                                    | 8/17 [00:35<00:29,  3.29s/it]
 # IoU 0: 0.951237, IoU 1: 0.710608, IoU 2: 0.465442, IoU 3: 0.558243, IoU 4: 0.634782, IoU 5: 0.005291, IoU 6: 0.441033
-
+# pixAcc: 0.7489, mIoU: 0.5204, fwIoU: 0.5999: 100%|������������������������������������������������������������������������������������������������������| 17/17 [00:49<00:00,  2.92s/it]
+# freq0: 0.061955, freq1: 0.078376, freq2: 0.163651, freq3: 0.357225, freq4: 0.298697, freq5: 0.016080, freq6: 0.024017                                    | 8/17 [00:38<00:32,  3.64s/it]
+# IoU 0: 0.947047, IoU 1: 0.646243, IoU 2: 0.508729, IoU 3: 0.583762, IoU 4: 0.641149, IoU 5: 0.041550, IoU 6: 0.274465
 # adam
 
 # circle loss, focal loss
