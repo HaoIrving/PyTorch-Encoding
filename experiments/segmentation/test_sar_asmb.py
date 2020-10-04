7d92275841019992c3988652d2732ed6361f9424
sun <sun@sun>
2020-10-04 20:02:13 +0800

    的不过

--- experiments/segmentation/test_sar_asmb.py
+++ experiments/segmentation/test_sar_asmb.py
@@ -142,10 +142,10 @@ def test(args):
     resume = [
         "best/psp_noise_6596.pth.tar",
         "best/psp_noise_6549.pth.tar",
-        "best/deeplab_noise_6272.pth.tar", 
-        "best/encnet_noise_6190.pth.tar", 
-        "best/psp_noise_6122.pth.tar",
-        "best/deeplab_noise_5999.pth.tar", 
+        # "best/deeplab_noise_6272.pth.tar", 
+        # "best/encnet_noise_6190.pth.tar", 
+        # "best/psp_noise_6122.pth.tar",
+        # "best/deeplab_noise_5999.pth.tar", 
         ]
 
     ioukeys = [path.split("/")[-1].split(".")[0] for path in resume]
